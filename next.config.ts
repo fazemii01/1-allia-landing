1789d6266acb4cbbe8df35483c8cd2b558d5d41f
fazemii01 <bestvatp@gmail.com>
2026-07-17 02:08:57 +0700

    nah id win

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  experimental: {
-    allowedDevOrigins: ['new.alliakids.com'],
-  },
+  allowedDevOrigins: ['new.alliakids.com'],
 };
 
 export default nextConfig;
