fc05333c298172ca0bb6db774e9059deea2c4cf0
fazemii01 <bestvatp@gmail.com>
2026-07-19 09:49:27 +0700

    bck2

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  allowedDevOrigins: ['new.alliakids.com'],
+  allowedDevOrigins: ['new.alliakids.com', 'alliakids.com'],
 };
 
 export default nextConfig;
